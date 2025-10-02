da9f6ee3ab04ab73b7b8c37158ba92465db915ca
johannesTbz <johanneslind02@gmail.com>
2025-10-02 18:28:30 +0200

    Reworked trees (can now fall)
    Can fell trees but they wont despawn for some reason? patch for that coming next

--- Scripts/tree.gd
+++ Scripts/tree.gd
@@ -1,21 +1,15 @@
-extends Node2D
+extends StaticBody2D
 
 var health = 100
 var player_InAttack_Zone = false
 var can_take_damage = true
+var is_falling: bool = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+@onready var anim: AnimationPlayer = $AnimationPlayer
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	deal_with_damage()
-
-func tree():
-	pass
-
 
 func _on_hurtbox_tree_body_entered(body: Node2D) -> void:
 	if body.has_method("player"):
@@ -33,7 +27,18 @@ func deal_with_damage():
 			can_take_damage = false
 			print("tree health = ", health)
 			if health <= 0:
-				self.queue_free()
+				start_fall()
+				
+func start_fall():
+	is_falling = true
+	anim.play("Fall")
+	$HurtboxTree.monitoring = false
+	$CollisionShape2D.disabled = true
+	
+func _on_AnimationPlayer_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Fall":
+		queue_free()
 	
 func _on_take_damage_cooldown_timeout() -> void:
 	can_take_damage = true
+	
